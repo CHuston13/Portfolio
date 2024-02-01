c0136ba997fec2488448b5555b42586cadc02566
CHuston13 <CDHprogrammer@gmail.com>
2024-02-01 12:09:24 -0600

    hey

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Projects = () => {
   return (
     <section id="projects">
-      <h2>Projects</h2>
+      <h2 id='ProjectHeader'>Projects</h2>
+      <a href="https://chuston13.github.io/NASA-APOD/">APOD</a>
+      <a href="https://chuston13.github.io/ant-simulator/"></a>
      
     </section>
   );
