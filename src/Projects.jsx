955c7992a8e907977246b61c1e13888828e43ec1
CHuston13 <CDHprogrammer@gmail.com>
2024-02-05 17:37:28 -0600

    new

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,11 +4,17 @@ const Projects = () => {
   return (
     <section id="projects">
       <h2 id='ProjectHeader'>Projects</h2>
-      <img src="src/Photos/ToDo.png"></img>
-      <img src='src/Photos/Apod.png'></img>
+      <img src="src/Photos/ToDo.png" id="Photo1"></img>
+      <a href=""></a>
+      <img src="src/Photos/Apod.png" id="Photo2"></img>
+      <>
+      <img src="src/Photos/WaveClone.png" id="Photo3"></img>
+      <a href="https://github.com/CHuston13/Wave">View Code</a>
+      </>
+      <>
       <a href="https://chuston13.github.io/NASA-APOD/">APOD</a>
       <a href="https://chuston13.github.io/ant-simulator/">Ant Simulator</a>
-     
+     </>
     </section>
   );
 };
