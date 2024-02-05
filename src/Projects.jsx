737a002e483b63d9d2e3ef4b6c8f1f6967845644
CHuston13 <CDHprogrammer@gmail.com>
2024-02-05 09:48:24 -0600

    new message

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,8 +4,10 @@ const Projects = () => {
   return (
     <section id="projects">
       <h2 id='ProjectHeader'>Projects</h2>
+      <img src="src/Photos/ToDo.png"></img>
+      <img src='src/Photos/Apod.png'></img>
       <a href="https://chuston13.github.io/NASA-APOD/">APOD</a>
-      <a href="https://chuston13.github.io/ant-simulator/"></a>
+      <a href="https://chuston13.github.io/ant-simulator/">Ant Simulator</a>
      
     </section>
   );
